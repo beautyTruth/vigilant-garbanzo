b6e328f84a2126294cfb2519820ac7ef5f6ee548
Phil <happy.melodist@gmail.com>
2022-02-05 17:34:10 -0600

    write the drawBoard function, parameters, and select the HTML elements

--- app copy.js
+++ app copy.js
@@ -20,19 +20,7 @@ function drawBoard() {
     let border = "";
 
     if (index < 3) {
-      border += "border-bottom: 2px solid maroon;";
-    }
-
-    if (index % 3 === 0) {
-      border += "border-right: 2px solid maroon;";
-    }
-
-    if (index % 3 === 2) {
-      border += "border-left: 2px solid maroon;";
-    }
-
-    if (index > 5) {
-      border += "border-top: 2px solid maroon;";
+      border += "border-bottom 2px solid maroon;";
     }
 
     box.style = border;
